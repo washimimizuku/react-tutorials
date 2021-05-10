a145a62c6514de619fbd0a16dffa3f7ce72f34bf
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-05-10 11:32:17 +0200

    Add props to GoalList component

--- academind-react-refresher/src/App.js
+++ academind-react-refresher/src/App.js
@@ -5,10 +5,16 @@ import GoalList from './components/GoalList';
 import './App.css'
 
 const App = () => {
+    const courseGoals = [
+        {id: 'cg1', text: "Finish the Course"},
+        {id: 'cg2', text: "Learn all about the Course Main Topic"},
+        {id: 'cg3', text: "Help other students in the Course Q&A"},
+    ];
+
     return (
         <div className="course-goals">
             <h2>Course Goals</h2>
-            <GoalList />
+            <GoalList goals={courseGoals} />
         </div>
     );
 };
--- academind-react-refresher/src/components/GoalList.js
+++ academind-react-refresher/src/components/GoalList.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 import './GoalList.css'
 
-const GoalList = () => {
+const GoalList = props => {
     return (
         <ul className="goal-list">
-            <li>Finish the Course</li>
-            <li>Learn all about the Course Main Topic</li>
-            <li>Help other students in the Course Q&amp;A</li>
+            { props.goals.map((goal) => {
+                return <li key={goal.id}>{goal.text}</li>
+            }) }
         </ul>
     );
-}
+};
 
 export default GoalList;
