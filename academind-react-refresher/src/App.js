39dd57831e34273a5a39a601c5b8b6180bd1e155
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-05-10 11:19:57 +0200

    Add GoalList component to react refresher

--- academind-react-refresher/src/App.js
+++ academind-react-refresher/src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
+import GoalList from './components/GoalList';
+
+import './App.css'
+
 const App = () => {
-  return <h1 title="This works!">Hi, <span>this</span> is ReactJS!</h1>;
+    return (
+        <div className="course-goals">
+            <h2>Course Goals</h2>
+            <GoalList />
+        </div>
+    );
 };
 
 export default App;
