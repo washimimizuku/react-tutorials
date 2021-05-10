ad6501040f9577cd891c0c6c86fa976d5988a9a6
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-05-10 11:53:05 +0200

    Add state to react refresher

--- academind-react-refresher/src/App.js
+++ academind-react-refresher/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import GoalList from './components/GoalList/GoalList';
 import NewGoal from './components/NewGoal/NewGoal';
@@ -6,14 +6,14 @@ import NewGoal from './components/NewGoal/NewGoal';
 import './App.css'
 
 const App = () => {
-    const courseGoals = [
+    const [courseGoals, setCourseGoals] = useState([
         {id: 'cg1', text: "Finish the Course"},
         {id: 'cg2', text: "Learn all about the Course Main Topic"},
         {id: 'cg3', text: "Help other students in the Course Q&A"},
-    ];
+    ]);
 
     const addNewGoalHandler = (newGoal) => {
-        courseGoals.push(newGoal);
+        setCourseGoals(courseGoals.concat(newGoal))
     }
 
     return (
