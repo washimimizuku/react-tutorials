c936ac3efed911f9debda811e65294568ca95ef0
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-05-10 12:34:48 +0200

    Add two way binding to the NewGoal component

--- academind-react-refresher/src/components/NewGoal/NewGoal.js
+++ academind-react-refresher/src/components/NewGoal/NewGoal.js
@@ -1,22 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './NewGoal.css'
 
 const NewGoal = props => {
+    const[ enteredText, setEnteredText ] = useState('');
+
     const addGoalHandler = event => {
         event.preventDefault();
 
         const newGoal = {
             id: Math.random().toString(),
-            text: 'My new goal!'
+            text: enteredText,
         };
+
+        setEnteredText('');
 
         props.onAddGoal(newGoal);
     };
 
+    const textChangeHandler = event => {
+        setEnteredText(event.target.value);
+    };
+
     return (
         <form className="new-goal" onSubmit={addGoalHandler}>
-            <input type="text" />
+            <input type="text" value={enteredText} onChange={textChangeHandler} />
             <button type="submit">Add Goal</button>
         </form>
     );
