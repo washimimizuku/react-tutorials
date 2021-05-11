2b81f08189f7cab9c640786ac597def1516fdd69
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-05-11 10:57:28 +0200

    Add change text button with state

--- rn-first-app/App.js
+++ rn-first-app/App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [outputText, setOutputText] = useState(
+    'Open up App.js to start working on your app!'
+  );
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{outputText}</Text>
+      <Button
+        title="Change Text"
+        onPress={() => setOutputText('The text changed!')}
+      />
       <StatusBar style="auto" />
     </View>
   );
