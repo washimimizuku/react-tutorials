ccb4a899a9ae90c9e4a073bffdd7cde579b182f4
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-05-11 14:40:57 +0200

    Add inline styling

--- udemy-react-native/rn-complete-guide/App.js
+++ udemy-react-native/rn-complete-guide/App.js
@@ -1,21 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{ padding: 50 }}>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
+        <TextInput
+          placeholder="Course Goal"
+          style={{
+            borderBottomColor: 'black',
+            borderBottomWidth: 1,
+            padding: 10,
+            width: '80%',
+          }}
+        />
+        <Button title="ADD" />
+      </View>
+      <View></View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
