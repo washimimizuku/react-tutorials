27e858035769b1d76f69c3a788c5d749c62aad71
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-05-11 15:02:56 +0200

    move styles to Stylesheet object

--- udemy-react-native/rn-complete-guide/App.js
+++ udemy-react-native/rn-complete-guide/App.js
@@ -4,23 +4,9 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{ padding: 50 }}>
-      <View
-        style={{
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-        }}
-      >
-        <TextInput
-          placeholder="Course Goal"
-          style={{
-            borderBottomColor: 'black',
-            borderBottomWidth: 1,
-            padding: 10,
-            width: '80%',
-          }}
-        />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Course Goal" style={styles.input} />
         <Button title="ADD" />
       </View>
       <View></View>
@@ -29,4 +15,19 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  input: {
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+    padding: 10,
+    width: '80%',
+  },
+});
