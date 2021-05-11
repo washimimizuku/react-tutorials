b222b2e3676328b067da37eeb48a994816abcd0e
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-05-11 15:19:31 +0200

    Add list of items

--- udemy-react-native/rn-complete-guide/App.js
+++ udemy-react-native/rn-complete-guide/App.js
@@ -25,7 +25,11 @@ export default function App() {
         />
         <Button title="ADD" onPress={addGoalHandler} />
       </View>
-      <View></View>
+      <View>
+        {courseGoals.map((goal) => (
+          <Text key={goal}>{goal}</Text>
+        ))}
+      </View>
       <StatusBar style="auto" />
     </View>
   );
