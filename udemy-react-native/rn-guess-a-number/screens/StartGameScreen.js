3b41ac84c21c2353861d4d5f22d33ce921290b51
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-05-12 15:04:42 +0200

    Style the select a number card

--- udemy-react-native/rn-guess-a-number/screens/StartGameScreen.js
+++ udemy-react-native/rn-guess-a-number/screens/StartGameScreen.js
@@ -31,6 +31,17 @@ const styles = StyleSheet.create({
     width: 300,
     maxWidth: '80%',
     alignItems: 'center',
+    shadowColor: 'black',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowRadius: 6,
+    shadowOpacity: 0.26,
+    elevation: 8,
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10,
   },
   buttonContainer: {
     flexDirection: 'row',
