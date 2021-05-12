655513bfcd70f4b2cdff3e62eb6790a506f0d5d5
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-05-12 09:35:29 +0200

    Add scrollable view to the list

--- udemy-react-native/rn-complete-guide/App.js
+++ udemy-react-native/rn-complete-guide/App.js
@@ -1,6 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  ScrollView,
+} from 'react-native';
 
 export default function App() {
   const [enteredGoal, setEnteredGoal] = useState('');
@@ -25,13 +32,13 @@ export default function App() {
         />
         <Button title="ADD" onPress={addGoalHandler} />
       </View>
-      <View>
+      <ScrollView>
         {courseGoals.map((goal) => (
           <View key={goal} style={styles.listItem}>
             <Text>{goal}</Text>
           </View>
         ))}
-      </View>
+      </ScrollView>
       <StatusBar style="auto" />
     </View>
   );
