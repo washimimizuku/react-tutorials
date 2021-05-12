02f8847e2177e0c49fdb9c7e9957e92a6910bcc1
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-05-12 14:51:22 +0200

    Add buttons to the start game screen

--- udemy-react-native/rn-guess-a-number/screens/StartGameScreen.js
+++ udemy-react-native/rn-guess-a-number/screens/StartGameScreen.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 
 const StartGameScreen = (props) => {
   return (
     <View style={styles.screen}>
-      <Text>The Game Screen</Text>
+      <Text style={styles.title}>Start a New Game!</Text>
+      <View style={styles.inputContainer}>
+        <Text>Select a Number</Text>
+        <TextInput />
+        <View style={styles.buttonContainer}>
+          <Button title="Reset" onPress={() => {}} />
+          <Button title="Confirm" onPress={() => {}} />
+        </View>
+      </View>
     </View>
   );
 };
@@ -14,6 +22,21 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 10,
     alignItems: 'center',
+  },
+  title: {
+    fontSize: 20,
+    marginVertical: 10,
+  },
+  inputContainer: {
+    width: 300,
+    maxWidth: '80%',
+    alignItems: 'center',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-between',
+    paddingHorizontal: 15,
   },
 });
 
