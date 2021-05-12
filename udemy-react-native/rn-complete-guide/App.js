683620bfd8d2b42b433b0c653a6d12a1a4b6cb8e
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-05-12 10:37:44 +0200

    Add delete item

--- udemy-react-native/rn-complete-guide/App.js
+++ udemy-react-native/rn-complete-guide/App.js
@@ -15,13 +15,25 @@ export default function App() {
     ]);
   };
 
+  const removeGoalHandler = (goalId) => {
+    setCourseGoals((currentGoals) => {
+      return currentGoals.filter((goal) => goal.uid != goalId);
+    });
+  };
+
   return (
     <View style={styles.screen}>
       <GoalInput onAddGoal={addGoalHandler} />
       <FlatList
         keyExtractor={(item, index) => item.uid}
         data={courseGoals}
-        renderItem={(itemData) => <GoalItem title={itemData.item.value} />}
+        renderItem={(itemData) => (
+          <GoalItem
+            uid={itemData.item.uid}
+            onDelete={removeGoalHandler}
+            title={itemData.item.value}
+          />
+        )}
       />
 
       <StatusBar style="auto" />
