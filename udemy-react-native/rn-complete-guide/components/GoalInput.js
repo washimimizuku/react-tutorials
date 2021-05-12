30b24f01f0a79bd81441887ca7cb857884d95ee6
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-05-12 11:09:30 +0200

    Restyle the modal

--- udemy-react-native/rn-complete-guide/components/GoalInput.js
+++ udemy-react-native/rn-complete-guide/components/GoalInput.js
@@ -22,8 +22,10 @@ const GoalInput = (props) => {
           onChangeText={goalInputHandler}
           value={enteredGoal}
         />
-        <Button title="CANCEL" color="red" onPress={props.onCancel} />
-        <Button title="ADD" onPress={addGoalHandler} />
+        <View style={styles.buttonContainer}>
+          <Button title="CANCEL" color="red" onPress={props.onCancel} />
+          <Button title="ADD" onPress={addGoalHandler} />
+        </View>
       </View>
     </Modal>
   );
@@ -41,6 +43,11 @@ const styles = StyleSheet.create({
     padding: 10,
     marginBottom: 10,
     width: '80%',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '40%',
   },
 });
 
