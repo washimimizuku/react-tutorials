2fe4f118d539abbf6e85a87cf09aeddbca1e7253
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-05-14 13:01:47 +0200

    Add GameOver screen component

--- udemy-react-native/rn-guess-a-number/screens/GameScreen.js
+++ udemy-react-native/rn-guess-a-number/screens/GameScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { View, Text, Button, Alert, StyleSheet } from 'react-native';
 
 import NumberContainer from '../components/NumberContainer';
@@ -21,9 +21,18 @@ const GameScreen = (props) => {
   const [currentGuess, setCurrentGuess] = useState(
     generateRandomBetween(1, 100, props.userChoice)
   );
+  const [rounds, setRounds] = useState(0);
 
   const currentLow = useRef(1);
   const currentHigh = useRef(100);
+
+  const { userChoice, onGameOver } = props;
+
+  useEffect(() => {
+    if (currentGuess === props.userChoice) {
+      props.onGameOver(rounds);
+    }
+  }, [currentGuess, userChoice, onGameOver]);
 
   const nextGuessHandler = (direction) => {
     if (
@@ -41,12 +50,14 @@ const GameScreen = (props) => {
     } else {
       currentLow.current = currentGuess;
     }
+
     const nextNumber = generateRandomBetween(
       currentLow.current,
       currentHigh.current,
       currentGuess
     );
     setCurrentGuess(nextNumber);
+    setRounds((currentRounds) => currentRounds + 1);
   };
 
   return (
