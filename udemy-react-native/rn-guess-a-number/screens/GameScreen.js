0581da4d06e8addc21c0043b15a2a3ee587ce227
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-05-14 12:14:57 +0200

    Add GameScreen component

---
+++ udemy-react-native/rn-guess-a-number/screens/GameScreen.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const generateRandomBetween = (min, max, exclude) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  const randomNumber = Math.floor(Math.random() * (max - min)) + min;
+
+  if (randomNumber === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return randomNumber;
+  }
+};
+
+const GameScreen = (props) => {
+  const [currentGuess, setCurrentGuess] = useState(
+    generateRandomBetween(1, 100, props.userChoice)
+  );
+  return <View></View>;
+};
+
+const styles = StyleSheet.create({});
+
+export default GameScreen;
