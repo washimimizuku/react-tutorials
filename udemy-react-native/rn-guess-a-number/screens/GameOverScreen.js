7d88fa0816f3220199bb8db6e4840405e058564f
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-05-14 15:25:06 +0200

    Add an image from the assets

--- udemy-react-native/rn-guess-a-number/screens/GameOverScreen.js
+++ udemy-react-native/rn-guess-a-number/screens/GameOverScreen.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { View, Button, StyleSheet } from 'react-native';
+import { View, Button, Image, StyleSheet } from 'react-native';
 
+import TitleText from '../components/TitleText';
 import BodyText from '../components/BodyText';
 
 const GameOverScreen = (props) => {
   return (
     <View style={styles.screen}>
-      <BodyText>The Game is Over</BodyText>
+      <TitleText>The Game is Over</TitleText>
+      <View style={styles.imageContainer}>
+        <Image
+          source={require('../assets/success.png')}
+          style={styles.image}
+          resizeMode="cover"
+        />
+      </View>
       <BodyText>Number of rounds: {props.roundsNumber}</BodyText>
       <BodyText>Number was: {props.userNumber}</BodyText>
       <Button title="NEW GAME" onPress={props.onRestart} />
@@ -19,6 +27,19 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: 'black',
+    overflow: 'hidden',
+    marginVertical: 30,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
   },
 });
 
