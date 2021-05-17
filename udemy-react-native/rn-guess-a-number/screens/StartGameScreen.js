6cdc03c100e327016c5dcab76a23a92e2bcf6d73
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-05-17 12:06:15 +0200

    Improve layout on small screens

--- udemy-react-native/rn-guess-a-number/screens/StartGameScreen.js
+++ udemy-react-native/rn-guess-a-number/screens/StartGameScreen.js
@@ -117,8 +117,9 @@ const styles = StyleSheet.create({
     fontFamily: 'open-sans-bold',
   },
   inputContainer: {
-    width: 300,
-    maxWidth: '80%',
+    width: '80%',
+    minWidth: 300,
+    maxWidth: '95%',
     alignItems: 'center',
   },
   buttonContainer: {
