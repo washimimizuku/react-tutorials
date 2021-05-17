97de0c0355bccb9298e754562b2643f010f7c0d3
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-05-17 13:03:05 +0200

    Improve layout on small screens

--- udemy-react-native/rn-guess-a-number/screens/GameOverScreen.js
+++ udemy-react-native/rn-guess-a-number/screens/GameOverScreen.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import { View, Button, Image, Text, StyleSheet } from 'react-native';
+import {
+  View,
+  Button,
+  Image,
+  Text,
+  StyleSheet,
+  Dimensions,
+  ScrollView,
+} from 'react-native';
 
 import TitleText from '../components/TitleText';
 import BodyText from '../components/BodyText';
@@ -8,31 +16,33 @@ import Colors from '../constants/colors';
 
 const GameOverScreen = (props) => {
   return (
-    <View style={styles.screen}>
-      <TitleText>The Game is Over</TitleText>
-      <View style={styles.imageContainer}>
-        <Image
-          source={require('../assets/success.png')}
-          /*
+    <ScrollView>
+      <View style={styles.screen}>
+        <TitleText>The Game is Over</TitleText>
+        <View style={styles.imageContainer}>
+          <Image
+            source={require('../assets/success.png')}
+            /*
           source={{
             uri: 'https://nypost.com/wp-content/uploads/sites/2/2019/07/travel-matterhorn-mountain.jpg?quality=90&strip=all&w=1236&h=820&crop=1',
           }}
           */
-          style={styles.image}
-          resizeMode="cover"
-          fadeDuration={1000}
-        />
+            style={styles.image}
+            resizeMode="cover"
+            fadeDuration={1000}
+          />
+        </View>
+        <View style={styles.resultContainer}>
+          <BodyText style={styles.resultText}>
+            Your phone needed{' '}
+            <Text style={styles.highlight}>{props.roundsNumber}</Text> rounds to
+            guess the number{' '}
+            <Text style={styles.highlight}>{props.userNumber}</Text>
+          </BodyText>
+        </View>
+        <MainButton onPress={props.onRestart}>NEW GAME</MainButton>
       </View>
-      <View style={styles.resultContainer}>
-        <BodyText style={styles.resultText}>
-          Your phone needed{' '}
-          <Text style={styles.highlight}>{props.roundsNumber}</Text> rounds to
-          guess the number{' '}
-          <Text style={styles.highlight}>{props.userNumber}</Text>
-        </BodyText>
-      </View>
-      <MainButton onPress={props.onRestart}>NEW GAME</MainButton>
-    </View>
+    </ScrollView>
   );
 };
 
@@ -42,28 +52,28 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  imageContainer: {
+    width: Dimensions.get('window').width * 0.7,
+    height: Dimensions.get('window').width * 0.7,
+    borderRadius: (Dimensions.get('window').width * 0.7) / 2,
+    borderWidth: 3,
+    borderColor: 'black',
+    overflow: 'hidden',
+    marginVertical: Dimensions.get('window').height / 30,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
   resultContainer: {
-    marginHorizontal: 50,
-    marginVertical: 15,
+    marginHorizontal: 30,
+    marginVertical: Dimensions.get('window').height / 60,
     justifyContent: 'center',
     alignItems: 'center',
   },
   resultText: {
     textAlign: 'center',
-    fontSize: 20,
-  },
-  imageContainer: {
-    width: 300,
-    height: 300,
-    borderRadius: 150,
-    borderWidth: 3,
-    borderColor: 'black',
-    overflow: 'hidden',
-    marginVertical: 30,
-  },
-  image: {
-    width: '100%',
-    height: '100%',
+    fontSize: Dimensions.get('window').height < 400 ? 16 : 20,
   },
   highlight: {
     color: Colors.primary,
--- udemy-react-native/rn-guess-a-number/screens/GameScreen.js
+++ udemy-react-native/rn-guess-a-number/screens/GameScreen.js
@@ -5,6 +5,7 @@ import {
   // ScrollView,
   FlatList,
   StyleSheet,
+  Dimensions,
 } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
@@ -89,6 +90,11 @@ const GameScreen = (props) => {
     ]);
   };
 
+  let listContainerStyle = styles.listContainer;
+  if (Dimensions.get('window').width < 350) {
+    listContainerStyle = styles.listContainerBig;
+  }
+
   return (
     <View style={styles.screen}>
       <TitleText>Opponent's Guess</TitleText>
@@ -101,7 +107,7 @@ const GameScreen = (props) => {
           <Ionicons name="md-add" size={25} color="white" />
         </MainButton>
       </Card>
-      <View style={styles.listContainer}>
+      <View style={listContainerStyle}>
         {/*
         <ScrollView contentContainerStyle={styles.list}>
           {pastGuesses.map((guess, index) =>
@@ -129,13 +135,17 @@ const styles = StyleSheet.create({
   buttonContainer: {
     flexDirection: 'row',
     justifyContent: 'space-around',
-    marginTop: 20,
+    marginTop: Dimensions.get('window').height > 600 ? 20 : 5,
     width: 400,
     maxWidth: '90%',
   },
   listContainer: {
     flex: 1,
     width: '60%',
+  },
+  listContainerBig: {
+    flex: 1,
+    width: '80%',
   },
   list: {
     flexGrow: 1,
