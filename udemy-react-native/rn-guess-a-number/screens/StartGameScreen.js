0796196b25fd8d87e25c70f83a007cec503a2871
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-05-17 12:10:02 +0200

    Use Dimensions

--- udemy-react-native/rn-guess-a-number/screens/StartGameScreen.js
+++ udemy-react-native/rn-guess-a-number/screens/StartGameScreen.js
@@ -6,6 +6,7 @@ import {
   TouchableWithoutFeedback,
   Keyboard,
   Alert,
+  Dimensions,
 } from 'react-native';
 
 import Card from '../components/Card';
@@ -129,7 +130,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 15,
   },
   button: {
-    width: 100,
+    width: Dimensions.get('window').width / 4,
   },
   input: {
     width: 50,
