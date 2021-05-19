c805c784d5c4c123b1b259e1c67a14b7cbd12e9e
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-05-19 20:50:20 +0200

    Use SafeAreaView

--- udemy-react-native/rn-guess-a-number/App.js
+++ udemy-react-native/rn-guess-a-number/App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, SafeAreaView } from 'react-native';
 import * as Font from 'expo-font';
 import AppLoading from 'expo-app-loading';
 
@@ -61,11 +61,11 @@ export default function App() {
   }
 
   return (
-    <View style={styles.screen}>
+    <SafeAreaView style={styles.screen}>
       <Header title="Guess a Number" />
       {content}
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
