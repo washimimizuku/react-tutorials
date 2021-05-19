bf91a65dc81d114501514d3a37de25ba389639cf
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-05-19 20:28:58 +0200

    Make header dependent on platform

--- udemy-react-native/rn-guess-a-number/components/Header.js
+++ udemy-react-native/rn-guess-a-number/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Platform } from 'react-native';
 
 import TitleText from './TitleText';
 import Colors from '../constants/colors';
@@ -17,12 +17,14 @@ const styles = StyleSheet.create({
     width: '100%',
     height: 90,
     paddingTop: 36,
-    backgroundColor: Colors.primary,
+    backgroundColor: Platform.OS === 'android' ? Colors.primary : 'white',
     alignItems: 'center',
     justifyContent: 'center',
+    borderBottomColor: Platform.OS === 'ios' ? '#ccc' : 'transparent',
+    borderBottomWidth: Platform.OS === 'ios' ? 1 : 0,
   },
   headerTitle: {
-    color: 'white',
+    color: Platform.OS === 'android' ? 'white' : Colors.primary,
     fontSize: 24,
   },
 });
