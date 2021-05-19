dc54fc16aad97999fa5d6ff8c12d3cc74655da68
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-05-19 20:37:30 +0200

    Make buttons platform specific

--- udemy-react-native/rn-guess-a-number/components/Header.js
+++ udemy-react-native/rn-guess-a-number/components/Header.js
@@ -6,22 +6,35 @@ import Colors from '../constants/colors';
 
 const Header = (props) => {
   return (
-    <View style={styles.header}>
+    <View
+      style={{
+        ...styles.headerBase,
+        ...Platform.select({
+          ios: styles.headerIOS,
+          android: styles.headerAndroid,
+        }),
+      }}
+    >
       <TitleText style={styles.headerTitle}>{props.title}</TitleText>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  header: {
+  headerBase: {
     width: '100%',
     height: 90,
     paddingTop: 36,
-    backgroundColor: Platform.OS === 'android' ? Colors.primary : 'white',
     alignItems: 'center',
     justifyContent: 'center',
-    borderBottomColor: Platform.OS === 'ios' ? '#ccc' : 'transparent',
-    borderBottomWidth: Platform.OS === 'ios' ? 1 : 0,
+  },
+  headerIOS: {
+    backgroundColor: 'white',
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1,
+  },
+  headerAndroid: {
+    backgroundColor: Colors.primary,
   },
   headerTitle: {
     color: Platform.OS === 'android' ? 'white' : Colors.primary,
