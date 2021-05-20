5e03de5a9801d0a700f1be95fa49d02dd25f21e8
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-05-20 16:08:46 +0200

    Add navigation

--- udemy-react-native/rn-the-meals/navigation/MealsNavigator.js
+++ udemy-react-native/rn-the-meals/navigation/MealsNavigator.js
@@ -2,7 +2,7 @@ import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
 import CategoriesScreen from '../screens/CategoriesScreen';
-import CategoriesMealsScreen from '../screens/CategoriesMealsScreen';
+import CategoriesMealsScreen from '../screens/CategoryMealsScreen';
 import MealDetailScreen from '../screens/MealDetailScreen';
 
 const MealsNavigator = createStackNavigator({
--- udemy-react-native/rn-the-meals/screens/CategoriesScreen.js
+++ udemy-react-native/rn-the-meals/screens/CategoriesScreen.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { Stylesheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Button } from 'react-native';
 
 const CategoriesScreen = (props) => {
   return (
     <View style={styles.screen}>
       <Text>The Categories Screen!</Text>
+      <Button
+        title="Go to Meals!"
+        onPress={() => {
+          props.navigation.navigate({ routeName: 'CategoryMeals' });
+        }}
+      />
     </View>
   );
 };
