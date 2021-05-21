cc80a4bbf4cff4f1fee4322708d865a4c4c8f056
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-05-21 12:24:18 +0200

    Grid Styling

--- udemy-react-native/rn-the-meals/screens/CategoriesScreen.js
+++ udemy-react-native/rn-the-meals/screens/CategoriesScreen.js
@@ -7,14 +7,17 @@ import {
   TouchableOpacity,
 } from 'react-native';
 
+import CategoryGridTile from '../components/CategoryGridTile';
+
 import { CATEGORIES } from '../data/dummy-data';
 
 const CategoriesScreen = (props) => {
   const renderGridItem = (itemData) => {
     return (
-      <TouchableOpacity
-        style={styles.gridItem}
-        onPress={() => {
+      <CategoryGridTile
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onSelect={() => {
           props.navigation.navigate({
             routeName: 'CategoryMeals',
             params: {
@@ -22,11 +25,7 @@ const CategoriesScreen = (props) => {
             },
           });
         }}
-      >
-        <View>
-          <Text>{itemData.item.title}</Text>
-        </View>
-      </TouchableOpacity>
+      />
     );
   };
 
@@ -49,11 +48,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-  },
-  gridItem: {
-    flex: 1,
-    margin: 15,
-    height: 150,
   },
 });
 
