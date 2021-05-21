066a5a6a434cedeb16d4d3087a8c1b0644bd788b
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-05-21 13:57:52 +0200

    Add navigation to meal detail screen

--- udemy-react-native/rn-the-meals/screens/CategoryMealsScreen.js
+++ udemy-react-native/rn-the-meals/screens/CategoryMealsScreen.js
@@ -14,7 +14,14 @@ const CategoryMealsScreen = (props) => {
         duration={itemData.item.duration}
         complexity={itemData.item.complexity}
         affordability={itemData.item.affordability}
-        onSelectMeal={() => {}}
+        onSelectMeal={() => {
+          props.navigation.navigate({
+            routeName: 'MealDetail',
+            params: {
+              mealId: itemData.item.id,
+            },
+          });
+        }}
       />
     );
   };
--- udemy-react-native/rn-the-meals/screens/MealDetailScreen.js
+++ udemy-react-native/rn-the-meals/screens/MealDetailScreen.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { StyleSheet, View, Text, Button } from 'react-native';
 
+import { MEALS } from '../data/dummy-data';
+
 const MealDetailScreen = (props) => {
+  const mealId = props.navigation.getParam('mealId');
+
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+
   return (
     <View style={styles.screen}>
-      <Text>The Meal Detail Screen!</Text>
+      <Text>{selectedMeal.title}</Text>
       <Button
         title="Go Back to Categories"
         onPress={() => {
@@ -13,6 +19,15 @@ const MealDetailScreen = (props) => {
       />
     </View>
   );
+};
+
+MealDetailScreen.navigationOptions = (navigationData) => {
+  const mealId = navigationData.navigation.getParam('mealId');
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+
+  return {
+    headerTitle: selectedMeal.title,
+  };
 };
 
 const styles = StyleSheet.create({
