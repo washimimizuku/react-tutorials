fb65f77404dbfab37d727a2a79c5e6ceda34b65e
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-05-25 11:40:57 +0200

    Add tab navigator

--- udemy-react-native/rn-the-meals/navigation/MealsNavigator.js
+++ udemy-react-native/rn-the-meals/navigation/MealsNavigator.js
@@ -2,10 +2,12 @@ import { Platform } from 'react-native';
 
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoriesMealsScreen from '../screens/CategoryMealsScreen';
 import MealDetailScreen from '../screens/MealDetailScreen';
+import FavoritesScreen from '../screens/FavoritesScreen';
 
 import Colors from '../constants/Colors';
 
@@ -36,4 +38,9 @@ const MealsNavigator = createStackNavigator(
   }
 );
 
-export default createAppContainer(MealsNavigator);
+const MealsFavNavigator = createBottomTabNavigator({
+  Meals: MealsNavigator,
+  Favorites: FavoritesScreen
+});
+
+export default createAppContainer(MealsFavNavigator);
