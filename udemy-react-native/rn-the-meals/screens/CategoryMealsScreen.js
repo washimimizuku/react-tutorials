981b54bd6e36b899b6bc342bfcf460fcdc653854
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-06-03 19:13:49 +0100

    Replace hardcoded meal list with redux version

--- udemy-react-native/rn-the-meals/screens/CategoryMealsScreen.js
+++ udemy-react-native/rn-the-meals/screens/CategoryMealsScreen.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-import { CATEGORIES, MEALS } from '../data/dummy-data';
+import { CATEGORIES } from '../data/dummy-data';
 import MealList from '../components/MealList';
 
 const CategoryMealScreen = (props) => {
   const catId = props.navigation.getParam('categoryId');
 
-  const displayedMeals = MEALS.filter(
+  const availableMeals = useSelector((state) => state.meals.filteredMeals);
+
+  const displayedMeals = availableMeals.filter(
     (meal) => meal.categoryIds.indexOf(catId) >= 0
   );
 
--- udemy-react-native/rn-the-meals/screens/MealDetailScreen.js
+++ udemy-react-native/rn-the-meals/screens/MealDetailScreen.js
@@ -1,15 +1,8 @@
-import React from 'react';
-import {
-  ScrollView,
-  View,
-  Image,
-  Text,
-  Button,
-  StyleSheet,
-} from 'react-native';
+import React, { useEffect } from 'react';
+import { ScrollView, View, Image, Text, StyleSheet } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import { useSelector } from 'react-redux';
 
-import { MEALS } from '../data/dummy-data';
 import HeaderButton from '../components/HeaderButton';
 import DefaultText from '../components/DefaultText';
 
@@ -22,9 +15,14 @@ const ListItem = (props) => {
 };
 
 const MealDetailScreen = (props) => {
+  const availableMeals = useSelector((state) => state.meals.meals);
   const mealId = props.navigation.getParam('mealId');
 
-  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+  const selectedMeal = availableMeals.find((meal) => meal.id === mealId);
+
+  useEffect(() => {
+    props.navigation.setParams({ mealTitle: selectedMeal.title });
+  }, [selectedMeal]);
 
   return (
     <ScrollView>
@@ -47,10 +45,13 @@ const MealDetailScreen = (props) => {
 };
 
 MealDetailScreen.navigationOptions = (navigationData) => {
-  const mealId = navigationData.navigation.getParam('mealId');
-  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+  // const mealId = navigationData.navigation.getParam('mealId');
+  // const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+
+  const mealTitle = navigationData.navigation.getParam('mealTitle');
+
   return {
-    headerTitle: selectedMeal.title,
+    headerTitle: mealTitle,
     headerRight: (
       <HeaderButtons HeaderButtonComponent={HeaderButton}>
         <Item
