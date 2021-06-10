4ccd0cf5448cd989d511bb0df38d128094d382a1
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-06-10 16:47:25 +0100

    Style product detail screen

--- udemy-react-native/rn-the-shop/screens/shop/ProductDetailScreen.js
+++ udemy-react-native/rn-the-shop/screens/shop/ProductDetailScreen.js
@@ -9,6 +9,8 @@ import {
 } from 'react-native';
 import { useSelector } from 'react-redux';
 
+import Colors from '../../constants/Colors';
+
 const ProductDetailScreen = (props) => {
   const productId = props.navigation.getParam('productId');
   const selectedProduct = useSelector((state) =>
@@ -16,9 +18,14 @@ const ProductDetailScreen = (props) => {
   );
 
   return (
-    <View style={styles.container}>
-      <Text>{selectedProduct.title}</Text>
-    </View>
+    <ScrollView>
+      <Image style={styles.image} source={{ uri: selectedProduct.imageUrl }} />
+      <View style={styles.actions}>
+        <Button color={Colors.primary} title="Add to Cart" onPress={() => {}} />
+      </View>
+      <Text style={styles.price}>${selectedProduct.price.toFixed(2)}</Text>
+      <Text style={styles.description}>{selectedProduct.description}</Text>
+    </ScrollView>
   );
 };
 
@@ -29,11 +36,24 @@ ProductDetailScreen.navigationOptions = (navData) => {
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  image: {
+    width: '100%',
+    height: 300,
+  },
+  actions: {
+    marginVertical: 10,
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  price: {
+    fontSize: 20,
+    color: '#888',
+    textAlign: 'center',
+    marginVertical: 20,
+  },
+  description: {
+    fontSize: 14,
+    textAlign: 'center',
+    marginHorizontal: 20,
   },
 });
 
