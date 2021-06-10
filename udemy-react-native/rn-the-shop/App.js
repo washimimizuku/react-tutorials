1296521f61f19908cc4634af880616ef6d3b2252
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-06-10 17:12:36 +0100

    Add fonts

--- udemy-react-native/rn-the-shop/App.js
+++ udemy-react-native/rn-the-shop/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { createStore, combineReducers } from 'redux';
+import { enableScreens } from 'react-native-screens';
 import { Provider } from 'react-redux';
+import AppLoading from 'expo-app-loading';
+import * as Font from 'expo-font';
 
 import productsProducer from './store/reducers/products';
 import ShopNavigator from './navigation/ShopNavigator';
+
+enableScreens();
 
 const rootReducer = combineReducers({
   products: productsProducer,
@@ -12,19 +17,29 @@ const rootReducer = combineReducers({
 
 const store = createStore(rootReducer);
 
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  });
+};
+
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={(err) => console.log(err)}
+      />
+    );
+  }
+
   return (
     <Provider store={store}>
       <ShopNavigator />
     </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- udemy-react-native/rn-the-shop/navigation/ShopNavigator.js
+++ udemy-react-native/rn-the-shop/navigation/ShopNavigator.js
@@ -16,6 +16,12 @@ const ProductNavigator = createStackNavigator(
       headerStyle: {
         backgroundColor: Platform.OS === 'android' ? Colors.primary : '',
       },
+      headerTitleStyle: {
+        fontFamily: 'open-sans-bold',
+      },
+      headerBackTitleStyle: {
+        fontFamily: 'open-sans',
+      },
       headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary,
     },
   }
