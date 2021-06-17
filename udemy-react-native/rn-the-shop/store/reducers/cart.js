ff046b10c1768064a132f8a940c50bf90c3d4d3a
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-06-17 10:52:36 +0100

    Clear cart after ordering

--- udemy-react-native/rn-the-shop/store/reducers/cart.js
+++ udemy-react-native/rn-the-shop/store/reducers/cart.js
@@ -1,4 +1,5 @@
 import { ADD_TO_CART, REMOVE_FROM_CART } from '../actions/cart';
+import { ADD_ORDER } from '../actions/orders';
 import CartItem from '../../models/cart';
 
 const initialState = {
@@ -57,6 +58,8 @@ export default (state = initialState, action) => {
         items: updatedCartItems,
         totalAmount: state.totalAmount - selectedCartItem.productPrice,
       };
+    case ADD_ORDER:
+      return initialState;
   }
 
   return state;
