e1dfae7b852402bd83ce5c8a7e62867622d22949
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-07-09 10:30:03 +0200

    Handle errors and add loading spinner

--- udemy-react-native/rn-the-shop/store/actions/products.js
+++ udemy-react-native/rn-the-shop/store/actions/products.js
@@ -8,25 +8,35 @@ export const SET_PRODUCTS = 'SET_PRODUCTS';
 
 export const fetchProducts = () => {
   return async (dispatch) => {
-    const response = await fetch(FIREBASE_URL + '/products.json');
+    // Any asyn code you want!
+    try {
+      const response = await fetch(FIREBASE_URL + '/products.json');
 
-    const resData = await response.json();
-    const loadedProducts = [];
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
 
-    for (const key in resData) {
-      loadedProducts.push(
-        new Product(
-          key,
-          'u1',
-          resData[key].title,
-          resData[key].imageUrl,
-          resData[key].description,
-          resData[key].price
-        )
-      );
+      const resData = await response.json();
+      const loadedProducts = [];
+
+      for (const key in resData) {
+        loadedProducts.push(
+          new Product(
+            key,
+            'u1',
+            resData[key].title,
+            resData[key].imageUrl,
+            resData[key].description,
+            resData[key].price
+          )
+        );
+      }
+
+      dispatch({ type: SET_PRODUCTS, products: loadedProducts });
+    } catch (err) {
+      // Send to custom analytics server.
+      throw err;
     }
-
-    dispatch({ type: SET_PRODUCTS, products: loadedProducts });
   };
 };
 
