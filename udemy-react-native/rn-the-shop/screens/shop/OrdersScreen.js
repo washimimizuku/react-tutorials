de32d9ea80a7f7d9be0fd20d5b03251534a7cfd6
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-07-14 10:49:33 +0100

    Fetch stored orders

--- udemy-react-native/rn-the-shop/screens/shop/OrdersScreen.js
+++ udemy-react-native/rn-the-shop/screens/shop/OrdersScreen.js
@@ -1,13 +1,40 @@
-import React from 'react';
-import { FlatList, Text, Platform } from 'react-native';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import {
+  View,
+  FlatList,
+  Platform,
+  ActivityIndicator,
+  StyleSheet,
+} from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import HeaderButton from '../../components/UI/HeaderButton';
 import OrderItem from '../../components/shop/OrderItem';
+import * as ordersActions from '../../store/actions/orders';
+import Colors from '../../constants/Colors';
 
 const OrdersScreen = (props) => {
+  const [isLoading, setIsLoading] = useState(false);
+
   const orders = useSelector((state) => state.orders.orders);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    setIsLoading(true);
+    dispatch(ordersActions.fetchOrders()).then(() => {
+      setIsLoading(false);
+    });
+  }, [dispatch]);
+
+  if (isLoading) {
+    return (
+      <View style={styles.centered}>
+        <ActivityIndicator size="large" color={Colors.primary} />
+      </View>
+    );
+  }
+
   return (
     <FlatList
       data={orders}
@@ -39,5 +66,12 @@ OrdersScreen.navigationOptions = (navData) => {
     ),
   };
 };
+const styles = StyleSheet.create({
+  centered: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default OrdersScreen;
