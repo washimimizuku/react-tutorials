8a475cb7f33281f94a9ad57fb581e0be17640c36
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-07-14 10:21:59 +0100

    Store orders on firebase

--- udemy-react-native/rn-the-shop/store/actions/orders.js
+++ udemy-react-native/rn-the-shop/store/actions/orders.js
@@ -1,8 +1,37 @@
+import { FIREBASE_URL } from '@env';
+
 export const ADD_ORDER = 'ADD_ORDER';
 
 export const addOrder = (cartItems, totalAmount) => {
-  return {
-    type: ADD_ORDER,
-    orderData: { items: cartItems, amount: totalAmount },
+  return async (dispatch) => {
+    const date = new Date();
+
+    const response = await fetch(FIREBASE_URL + '/orders/u1.json', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        cartItems: cartItems,
+        totalAmount: totalAmount,
+        date: date,
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error('Something went wrong!');
+    }
+
+    const resData = await response.json();
+
+    dispatch({
+      type: ADD_ORDER,
+      orderData: {
+        id: resData.name,
+        items: cartItems,
+        amount: totalAmount,
+        date: date,
+      },
+    });
   };
 };
