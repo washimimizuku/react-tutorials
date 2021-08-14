8c0fd282143ffd724c08d78df0b0c2ecd8022609
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-08-14 16:25:42 +0100

    Add user login

--- udemy-react-native/rn-the-shop/store/actions/auth.js
+++ udemy-react-native/rn-the-shop/store/actions/auth.js
@@ -1,6 +1,7 @@
 import { FIREBASE_WEB_API_KEY } from '@env';
 
 export const SIGNUP = 'SIGNUP';
+export const LOGIN = 'LOGIN';
 
 export const signup = (email, password) => {
   return async (dispatch) => {
@@ -28,5 +29,34 @@ export const signup = (email, password) => {
     console.log(resData);
 
     dispatch({ type: SIGNUP });
+  };
+};
+
+export const login = (email, password) => {
+  return async (dispatch) => {
+    const response = await fetch(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' +
+        FIREBASE_WEB_API_KEY,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }),
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error('Something went wrong!');
+    }
+
+    const resData = await response.json();
+    console.log(resData);
+
+    dispatch({ type: LOGIN });
   };
 };
