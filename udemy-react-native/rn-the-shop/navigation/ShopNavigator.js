d75c3422d610c922294c55491a95083fc7c12182
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-08-14 18:28:07 +0100

    Implement auto login

--- udemy-react-native/rn-the-shop/navigation/ShopNavigator.js
+++ udemy-react-native/rn-the-shop/navigation/ShopNavigator.js
@@ -13,6 +13,7 @@ import OrdersScreen from '../screens/shop/OrdersScreen';
 import UserProductsScreen from '../screens/user/UserProductsScreen';
 import EditProductScreen from '../screens/user/EditProductScreen';
 import AuthScreen from '../screens/user/AuthScreen';
+import StartupScreen from '../screens/StartupScreen';
 
 import Colors from '../constants/Colors';
 
@@ -109,6 +110,7 @@ const AuthNavigator = createStackNavigator(
 );
 
 const MainNavigator = createSwitchNavigator({
+  Start: StartupScreen,
   Auth: AuthNavigator,
   Shop: ShopNavigator,
 });
--- udemy-react-native/rn-the-shop/store/actions/auth.js
+++ udemy-react-native/rn-the-shop/store/actions/auth.js
@@ -1,7 +1,13 @@
 import { FIREBASE_WEB_API_KEY } from '@env';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const SIGNUP = 'SIGNUP';
 export const LOGIN = 'LOGIN';
+export const AUTHENTICATE = 'AUTHENTICATE';
+
+export const authenticate = (userId, token) => {
+  return { type: AUTHENTICATE, userId: userId, token: token };
+};
 
 export const signup = (email, password) => {
   return async (dispatch) => {
@@ -36,7 +42,12 @@ export const signup = (email, password) => {
     const resData = await response.json();
     console.log(resData);
 
-    dispatch({ type: SIGNUP, token: resData.idToken, userId: resData.localId });
+    dispatch(authenticate(resData.localId, resData.idToken));
+
+    const expirationDate = new Date(
+      new Date().getTime() + parseInt(resData.expiresIn) * 1000
+    );
+    saveDataToStorage(resData.idToken, resData.localId, expirationDate);
   };
 };
 
@@ -75,6 +86,22 @@ export const login = (email, password) => {
     const resData = await response.json();
     console.log(resData);
 
-    dispatch({ type: LOGIN, token: resData.idToken, userId: resData.localId });
+    dispatch(authenticate(resData.localId, resData.idToken));
+
+    const expirationDate = new Date(
+      new Date().getTime() + parseInt(resData.expiresIn) * 1000
+    );
+    saveDataToStorage(resData.idToken, resData.localId, expirationDate);
   };
+};
+
+const saveDataToStorage = (token, userId, expirationDate) => {
+  AsyncStorage.setItem(
+    'userData',
+    JSON.stringify({
+      token: token,
+      userId: userId,
+      expiryDate: expirationDate,
+    })
+  );
 };
