364c4eeeaa0a9b8e0cd15fe9240804bc16fe6efb
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-08-15 09:55:11 +0100

    Add form to add place screen

--- udemy-react-native/rn-great-places/screens/NewPlaceScreen.js
+++ udemy-react-native/rn-great-places/screens/NewPlaceScreen.js
@@ -1,11 +1,41 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  ScrollView,
+  Button,
+} from 'react-native';
+
+import Colors from '../constants/Colors';
 
 const NewPlaceScreen = (props) => {
+  const [titleValue, setTitleValue] = useState('');
+
+  const titleChangeHandler = (text) => {
+    // We could add validation
+    setTitleValue(text);
+  };
+
+  const savePlaceHanfler = () => {};
+
   return (
-    <View>
-      <Text>NewPlaceScreen</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.form}>
+        <Text style={styles.lable}>Title</Text>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={titleChangeHandler}
+          value={titleValue}
+        />
+        <Button
+          title="Save Place"
+          color={Colors.primary}
+          onPress={savePlaceHanfler}
+        />
+      </View>
+    </ScrollView>
   );
 };
 
@@ -13,6 +43,21 @@ NewPlaceScreen.navigationOptions = {
   headerTitle: 'Add Place',
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  form: {
+    margin: 30,
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 15,
+  },
+  textInput: {
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1,
+    marginBottom: 15,
+    paddingVertical: 4,
+    paddingHorizontal: 2,
+  },
+});
 
 export default NewPlaceScreen;
