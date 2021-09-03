fefb63f7f33db8c801320ee45969d79cba9b92ce
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-09-03 08:46:49 +0100

    store data no sqlite

--- udemy-react-native/rn-great-places/helpers/db.js
+++ udemy-react-native/rn-great-places/helpers/db.js
@@ -6,10 +6,29 @@ export const init = () => {
   const promise = new Promise((resolve, reject) => {
     db.transaction((tx) => {
       tx.executeSql(
-        'CREATE TABLE IF NOT EXISTS places (id INTEGER PRIMARY KEY NOT NULL, title TEXT NOT NULL, imageUri TEXT NOT NULL, address TEXT NOT NULL, lat REAL NOT NULL, lon REAL NOT NULL,lng REAL NOT NULL)',
+        'CREATE TABLE IF NOT EXISTS places (id INTEGER PRIMARY KEY NOT NULL, title TEXT NOT NULL, imageUri TEXT NOT NULL, address TEXT NOT NULL, lat REAL NOT NULL, lon REAL NOT NULL,lng REAL NOT NULL);',
         [],
         () => {
           resolve();
+        },
+        (_, err) => {
+          reject(err);
+        }
+      );
+    });
+  });
+
+  return promise;
+};
+
+export const insertPlace = (title, imageUri, address, lat, lng) => {
+  const promise = new Promise((resolve, reject) => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        'INSERT INTO places (title, imageUri, address, lat, lng) VALUES (?, ?, ?, ?, ?);',
+        [title, imageUri, address, lat, lng],
+        (_, result) => {
+          resolve(result);
         },
         (_, err) => {
           reject(err);
--- udemy-react-native/rn-great-places/store/places-actions.js
+++ udemy-react-native/rn-great-places/store/places-actions.js
@@ -1,5 +1,7 @@
 import * as FileSystem from 'expo-file-system';
 
+import { insertPlace } from '../helpers/db';
+
 export const ADD_PLACE = 'ADD_PLACE';
 
 export const addPlace = (title, image) => {
@@ -12,11 +14,24 @@ export const addPlace = (title, image) => {
         from: image,
         to: newPath,
       });
+
+      const dbResult = await insertPlace(
+        title,
+        newPath,
+        'Dummy address',
+        15.6,
+        12.3
+      );
+
+      console.log(dbResult);
+
+      dispatch({
+        type: ADD_PLACE,
+        placeData: { id: dbResult.insertId, title: title, image: newPath },
+      });
     } catch (err) {
       console.log(err);
       throw err;
     }
-
-    dispatch({ type: ADD_PLACE, placeData: { title: title, image: newPath } });
   };
 };
