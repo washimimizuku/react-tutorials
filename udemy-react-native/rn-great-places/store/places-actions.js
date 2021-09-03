db43214f491492f26e84b04c4c4615e9eb580b4b
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-09-03 08:58:08 +0100

    fetch places from sqlite

--- udemy-react-native/rn-great-places/store/places-actions.js
+++ udemy-react-native/rn-great-places/store/places-actions.js
@@ -1,8 +1,9 @@
 import * as FileSystem from 'expo-file-system';
 
-import { insertPlace } from '../helpers/db';
+import { insertPlace, fetchPlaces } from '../helpers/db';
 
 export const ADD_PLACE = 'ADD_PLACE';
+export const SET_PLACES = 'SET_PLACES';
 
 export const addPlace = (title, image) => {
   return async (dispatch) => {
@@ -31,6 +32,20 @@ export const addPlace = (title, image) => {
       });
     } catch (err) {
       console.log(err);
+      throw err;
+    }
+  };
+};
+
+export const loadPlaces = () => {
+  return async (dispatch) => {
+    try {
+      const dbResult = await fetchPlaces();
+
+      console.log(dbResult);
+
+      dispatch({ type: SET_PLACES, places: dbResult.rows._array });
+    } catch (err) {
       throw err;
     }
   };
--- udemy-react-native/rn-great-places/store/places-reducers.js
+++ udemy-react-native/rn-great-places/store/places-reducers.js
@@ -1,4 +1,4 @@
-import { ADD_PLACE } from './places-actions';
+import { ADD_PLACE, SET_PLACES } from './places-actions';
 import Place from '../models/place';
 
 const initialState = {
@@ -7,6 +7,12 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case SET_PLACES:
+      return {
+        places: action.places.map(
+          (pl) => new Place(pl.id.toString(), pl.title, pl.imageUri)
+        ),
+      };
     case ADD_PLACE:
       const newPlace = new Place(
         action.placeData.id,
