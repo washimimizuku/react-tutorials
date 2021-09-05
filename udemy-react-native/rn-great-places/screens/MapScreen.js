634842a04194981c05aa2b2afff98237f52916f4
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-09-05 19:43:15 +0100

    Make picked location saveable

--- udemy-react-native/rn-great-places/screens/MapScreen.js
+++ udemy-react-native/rn-great-places/screens/MapScreen.js
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect, useCallback } from 'react';
+import { TouchableOpacity, Text, Platform, StyleSheet } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
+
+import Colors from '../constants/Colors';
 
 const MapScreen = (props) => {
   const [selectedLocation, setSelectedLocation] = useState();
@@ -18,6 +20,18 @@ const MapScreen = (props) => {
       lng: event.nativeEvent.coordinate.longitude,
     });
   };
+
+  const savePickedLocationHandler = useCallback(() => {
+    if (!selectedLocation) {
+      // Could show an alert!
+      return;
+    }
+    props.navigation.navigate('NewPlace', { pickedLocation: selectedLocation });
+  }, [selectedLocation]);
+
+  useEffect(() => {
+    props.navigation.setParams({ saveLocation: savePickedLocationHandler });
+  }, [savePickedLocationHandler]);
 
   let markerCoordinates;
 
@@ -41,9 +55,28 @@ const MapScreen = (props) => {
   );
 };
 
+MapScreen.navigationOptions = (navData) => {
+  const saveFunction = navData.navigation.getParam('saveLocation');
+
+  return {
+    headerRight: (
+      <TouchableOpacity style={styles.headerButton} onPress={saveFunction}>
+        <Text style={styles.headerButtonText}>Save</Text>
+      </TouchableOpacity>
+    ),
+  };
+};
+
 const styles = StyleSheet.create({
   map: {
     flex: 1,
+  },
+  headerButton: {
+    marginHorizontal: 20,
+  },
+  headerButtonText: {
+    fontSize: 16,
+    color: Platform.OS === 'android' ? 'white' : Colors.primary,
   },
 });
 
