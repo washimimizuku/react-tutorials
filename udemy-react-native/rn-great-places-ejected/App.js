0eef153799b708f78c7f5ade6dc4ca1dd5ca3fc6
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-09-07 16:34:35 +0100

    Add project from great places project but ejected

---
+++ udemy-react-native/rn-great-places-ejected/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import ReduxThunk from 'redux-thunk';
+
+import PlacesNavigator from './navigation/PlacesNavigator';
+import placesReducer from './store/places-reducers';
+import { init } from './helpers/db';
+
+init()
+  .then(() => {
+    console.log('Initalized database.');
+  })
+  .catch(() => {
+    console.log('Initalizing database failed.');
+  });
+
+const rootReducer = combineReducers({
+  places: placesReducer,
+});
+
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <PlacesNavigator />
+    </Provider>
+  );
+}
